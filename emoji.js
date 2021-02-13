2ef01c53b5e992a52a7d0fe8ff6bdbb52abbd63d
Gabriel Rezende <g@brielrezen.de>
2021-02-13 18:56:09 -0300

    everthing upgraded

--- emoji.js
+++ emoji.js
@@ -1,13 +1,55 @@
-//Verificar se está rodando no domínio certo
+// Copy button
+// Option to put a space between emoticons texts
+// Verificar se está rodando no domínio certo
 
-let emoji = () => document.getElementById('emoji').value;
+const emoticons = {
+  angel: {
+    txt: "(a)",
+    img: "./emoticon/faces/angel_smile.gif"
+  },
+  happy: {
+    txt: ":)",
+    img: "./emoticon/faces/regular_smile.gif"
+  },
+  teethSmile: {
+    txt: ":-D",
+    img: "./emoticon/faces/teeth_smile.gif"
+  }
+}
 
-// console.log("😀")npm
+const listaEmoji = {
+  '😇': emoticons.angel,
+  '🙂': emoticons.happy,
+  '🙃': emoticons.happy,
+  '☺️': emoticons.happy,
+  '😊': emoticons.happy,
+  '🥲 ': emoticons.happy,
+  '☺️ ': emoticons.happy,
+  '😀': emoticons.teethSmile,
+  '😃': emoticons.teethSmile,
+  '😄': emoticons.teethSmile,
+  '😁': emoticons.teethSmile,
+  '😆': emoticons.teethSmile,
+  '😅': emoticons.teethSmile,
+  '😅': emoticons.teethSmile,
+  '😂': emoticons.teethSmile,
+  '🤣': emoticons.teethSmile,
+  '🥲': emoticons.teethSmile,
+}
 
 const mostraConteudo = () => {
-  if (document.getElementById('emoji').value == "😇") {
-    document.getElementById('result').innerText = '(a)'
-    document.getElementById('resultEmoticon').innerHTML = '<img src="./emoticon/faces/angel_smile.gif" alt="">';
-    console.log('Feliz')
-  }
+  let inputEmojis = splitter.splitGraphemes(document.getElementById('emoji').value)
+  let divEmoticons = '';
+  let divTxt = '';
+
+  inputEmojis.forEach(emoji => {
+    for (let prop in listaEmoji) {
+      if (prop == emoji) {
+        divEmoticons += `<img src=${listaEmoji[prop].img}>`
+        divTxt += " " + listaEmoji[prop].txt; // Test whether space is really needed
+      }
+    }
+    document.getElementById('resultEmoticon').innerHTML = divEmoticons;
+    document.getElementById('resultText').innerText = divTxt;
+  });
 }
